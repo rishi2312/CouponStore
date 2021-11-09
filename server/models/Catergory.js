aebd73da47d6f7192144f348e1d01cb23a216388
rajarshi sahu <rajarshi.sahu@hotmail.com>
2021-11-10 02:57:58 +0530

    Delete server directory

--- server/models/Catergory.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-const CategorySchema = mongoose.Schema({
-    category: String,
-    subcat: mongoose.Schema.Types.Mixed
-})
-
-export default mongoose.model('categories', CategorySchema)
